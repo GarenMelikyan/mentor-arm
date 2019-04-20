07e37b676ed1681e82e601bb508c25ca3f72568e
William Arustamyan <williamarustamyan@workfront.com>
2019-04-20 17:46:13 +0400

    Spring context and dependency configuration

--- mentor-arm-api/api-web/src/main/java/com/mentorarm/MentorArmeniaApplication.java
+++ mentor-arm-api/api-web/src/main/java/com/mentorarm/MentorArmeniaApplication.java
@@ -2,8 +2,12 @@ package com.mentorarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ImportResource("classpath:/com/mentorarm/mentorarm-api-web-context.xml")
 @SpringBootApplication
+//@EnableJpaRepositories("com.mentorarm.repositories")
 public class MentorArmeniaApplication {
 
     public static void main(String[] args) {
