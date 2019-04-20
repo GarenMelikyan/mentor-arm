504463fa1ac00502463814e32f5f59279bc4340e
William Arustamyan <williamarustamyan@workfront.com>
2019-04-20 17:16:49 +0400

    Restructuring project. Moved from gradle to maven

---
+++ mentor-arm-api/api-web/src/main/java/com/mentorarm/MentorArmeniaApplication.java
@@ -0,0 +1,13 @@
+package com.mentorarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorArmeniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorArmeniaApplication.class, args);
+    }
+
+}
